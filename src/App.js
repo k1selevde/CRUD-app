46881207b2f68f8a8cc9cb6f70ed95b3276f901c
Denis Kiselev <k1selevde@yandex.ru>
2020-03-28 22:12:25 +0500

    init project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import UserForm from "./forms/UserForm";
+import UserTable from "./table/UserTable";
+
+const App  = () => {
+    return (
+        <div>
+            <UserForm/>
+            <UserTable/>
+        </div>
+    );
+}
+
+export default App;
+
