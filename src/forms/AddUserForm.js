60669439f18a1e4eea02d5fe021a401644a5e596
Denis Kiselev <k1selevde@yandex.ru>
2020-03-29 10:58:20 +0500

    use createRef in AddForm

--- src/forms/AddUserForm.js
+++ src/forms/AddUserForm.js
@@ -1,34 +1,26 @@
-import React, {useState} from 'react'
+import React, {createRef, useState} from 'react'
 
 const AddUserForm = (props) => {
-    const defaultFormState = {id: null, name: '', username: ''};
-    const [user, setUser] = useState(defaultFormState);
-
-    const inputChangeHandler = e => {
-        const {name, value} = e.target;
-        setUser(
-            {...user, [name]: value}
-        )
-    }
+    const name = createRef(null);
+    const username = createRef(null);
 
     const submitHandler = (e) => {
         e.preventDefault();
-        if (!user.name || !user.username) return;
-        props.AddUser(user);
-        setUser(defaultFormState);
+        if (!name || !username) return;
+        props.AddUser({id: null, name: name.current.value, username: username.current.value});
+        name.current.value = '';
+        username.current.value = '';
     }
 
     return (
         <form onSubmit={submitHandler}>
             <div className="form-group">
                 <label htmlFor="name1">name</label>
-                <input type="text" className="form-control" id="name1" name="name" onChange={inputChangeHandler}
-                       value={user.name}/>
+                <input ref={name} type="text" className="form-control" id="name1" name="name"/>
             </div>
             <div className="form-group">
                 <label htmlFor="username1">username</label>
-                <input type="text" className="form-control" id="username1" name="username" onChange={inputChangeHandler}
-                       value={user.username}/>
+                <input ref={username} type="text" className="form-control" id="username1" name="username" />
             </div>
             <button type="submit" className="btn btn-primary">Добавить</button>
         </form>
